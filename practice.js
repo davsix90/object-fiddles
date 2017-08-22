8465cd0ffdb9b1b37644a2f4dccd823937a6cc6d
David <david@thehuemedia.com>
2017-08-22 13:18:14 -0600

    updated

--- practice.js
+++ practice.js
@@ -4,7 +4,12 @@
 //Create an object called me. Give it a key of name with the value being your name, and another key of age with the value being your age. Then alert your name using dot notation.
 
   //Code here
+var me = {
+  name : 'Dave',
+  age : 27,
+};
 
+alert(me.name);
 
 
 
@@ -18,7 +23,14 @@
 //Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to those keys be your favorite thing in that category.
 
   //Code here
-
+var favoriteThings = {
+  band : 'Linkin Park',
+  food : 'Tacos',
+  person : 'Lily Sixta',
+  book : 'Leadership and Self Deception',
+  movie : 'So I Married an Axe Murderer',
+  holiday : 'Christmas'
+};
 
 
 
@@ -28,7 +40,8 @@
 //After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 
   //Code here
-
+favoriteThings.car = 'Dodge Ram';
+favoriteThings.brand = 'Under Armor';
 
 
 
@@ -36,7 +49,8 @@
 //Now change the value of the food key in your favoriteThings object to be 'Chicken Nuggets' and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 
   //Code here
-
+favoriteThings.food = 'Chicken Nuggets';
+favoriteThings.book = 'Harry Potter';
 
 
 
@@ -56,7 +70,13 @@ that is named color, with the value being the color of your backpack.
 */
 
   //Code here
+var backPack = {
 
+};
+
+var item = 'firstPocket';
+backPack['firstPocket'] = 'chapstick';
+backPack.color = blue;
 
 
 
@@ -65,7 +85,7 @@ that is named color, with the value being the color of your backpack.
 //After you do the above, alert your entire backPack object.
 
   //Code here
-
+alert(backPack);
  
 
 /*
@@ -73,9 +93,7 @@ You probably noticed that it just alerted [object Object].
 Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console. 
 */
-
-  //Code here
-
+console.log(backPack);
 
 
 
@@ -97,7 +115,8 @@ var user2 = {
 // name -> 'Tyler S. McGinnis', email -> 'tyler.mcginnis@devmounta.in'. Make that change.
 
   //Code Here
-
+user2.name = 'Tyler S. McGinnis';
+user2.email = 'tyler.mcginnis@devmounta.in';
 
 
 
@@ -119,7 +138,9 @@ var user2 = {
 //Create an empty object called methodCollection.
 
   //Code Here
+var methodCollection = {
 
+}
 
 
 
@@ -130,7 +151,8 @@ object. One called 'alertHello' which alerts 'hello' and another method called l
  */
 
   //Code Here
-
+methodCollection.alertHello = alert('hello');
+methodCollection.logHello = console.log('hello');
 
 
 
